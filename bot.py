4bd66260c43888fd0e62d61bba10e141f0e8ed38
Abir Hasan <hackitabirhasan@gmail.com>
2023-10-13 01:33:06 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=SESSION,
+            name="pyro-bot",
+            # session_string=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
